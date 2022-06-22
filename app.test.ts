40545e6cc7afd8195615f23759c023a4647ff12a
jakub-c <nawias@gmail.com>
2022-06-22 21:35:28 +0000

    refactor tests using beforeEach, it

--- app.test.ts
+++ app.test.ts
@@ -1,14 +1,18 @@
 import { assertObjectMatch } from "https://deno.land/std@0.144.0/testing/asserts.ts";
+import { beforeEach, it } from "https://deno.land/std@0.144.0/testing/bdd.ts";
 import { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
 
 import { app } from "./app.ts";
 
-Deno.test("/ should point player to the yard", async () => {
+let request: any;
+beforeEach(async () => {
+  request = await superoak(app);
+});
+
+it("/ should point player to the yard", async () => {
   const expected = { "name": "The yard" };
 
-  const request = await superoak(app);
-
-  await request.get("/").expect(200).then((rsp) => {
-    assertObjectMatch(rsp.body, expected);
+  await request.get("/").expect(200).then((response: any) => {
+    assertObjectMatch(response.body, expected);
   });
 });
