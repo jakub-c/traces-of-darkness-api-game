0cf81f4eb10cb742e8519c100910605cad2b3fa2
jakub-c <nawias@gmail.com>
2022-08-26 21:00:48 +0000

    refactor: improve description of The Yard map location

--- src/map.ts
+++ src/map.ts
@@ -23,7 +23,8 @@ const house: mapLocation = {
 export const yard: mapLocation = {
   "endpoint": "",
   "name": "The yard",
-  "description": "....",
+  "description":
+    "It's a cold evening somewhere in New England. You are standing on the premises of a mansion that seems neither inhabited nor abandoned. The place feels like it's frozen in time. Did anyone ever live here? The unusualness of the situation makes you ponder turning away, going back to the Ford Model T parked just 30 yards from where you stand. Yet your thirst for knowledge pushes you forward. You realise you just took a step bringing you closer to the unknown. Is it a coincidence that you finished reading Faust just a few days ago? Goethe was a Freemason after all... Would he approve your quest?",
   "image":
     "https://tile.loc.gov/storage-services/service/pnp/fsa/8b23000/8b23400/8b23404v.jpg",
   "connections": [house],
